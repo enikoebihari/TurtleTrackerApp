44248de49262a1c9155a874dd9688d70dbcf7f38
Enikoe Bihari <enikoebihari@gmail.com>
2021-09-22 22:27:31 -0400

    while loop

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -15,15 +15,16 @@ file_name = './data/raw/sara.txt'
 #Create a file object from the file
 file_object = open(file_name,'r')
 
-#Read contents of file into a list
-line_list = file_object.readlines()
+#Read the first line in the file
+lineString = file_object.readline()
 
-#Close the file
-file_object.close()
+#Iterate with while loop
+while lineString:
 
-#Iterate through all lines in linelist
-for lineString in line_list:
-    if lineString[0] in ("#",'u'): 
+    #Advance to the next line
+    if lineString[0] in ("#",'u'):
+                 #Read the next line
+                 lineString = file_object.readline()
                  continue
 
     #Split the string into a list of data items
@@ -37,8 +38,46 @@ for lineString in line_list:
     obs_lon = lineData[7]    # Observation Longitude
     
     print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    
+    #Read the next line
+    lineString = file_object.readline()
+
+#Close the file
+file_object.close()
 
 # =============================================================================
+#
+# #Create a variable point to the data file
+# file_name = './data/raw/sara.txt'
+# 
+# #Create a file object from the file
+# file_object = open(file_name,'r')
+# 
+# #Read contents of file into a list
+# line_list = file_object.readlines()
+# 
+# #Close the file
+# file_object.close()
+# 
+# #Iterate through all lines in linelist
+# for lineString in line_list:
+#     if lineString[0] in ("#",'u'): 
+#                  continue
+# 
+#     #Split the string into a list of data items
+#     lineData = lineString.split()
+#     
+#     #Extract the items in list into variables
+#     record_id = lineData[0]  # ARGOS tracking record ID
+#     obs_date = lineData[2]   # Observation date
+#     ob_lc = lineData[4]      # Observation Location Class
+#     obs_lat = lineData[6]    # Observation Latitude
+#     obs_lon = lineData[7]    # Observation Longitude
+#     
+#     print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+# 
+# =============================================================================
+#
 # #Create a variable pointing to the data file
 # file_name = './data/raw/sara.txt'
 # 
@@ -86,6 +125,5 @@ for lineString in line_list:
 # 
 # # Print information to the use
 # print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
-# 
 # 
 # =============================================================================
