f132dd187ac1972fe3882823ecf305aa72cfaf3c
Enikoe Bihari <enikoebihari@gmail.com>
2021-09-23 15:17:21 -0400

    filters out lc not in 1,2,3

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,23 +9,24 @@
 # Date:   Fall 2021
 #-------------------------------------------------------------
 
-#Create a variable pointing to the data file
+#Create a variable point to the data file
 file_name = './data/raw/sara.txt'
 
 #Create a file object from the file
-file_object = open(file_name,'r')
+file_object = open(file_name,'r') 
+#Read contents of file into a list
+line_list = file_object.readlines()
 
-#Read the first line in the file
-lineString = file_object.readline()
+#Close the file
+file_object.close()
 
-#Iterate with while loop
-while lineString:
+#Create two empty dictionary objects
+date_dict = {}
+coord_dict = {}
 
-<<<<<<< HEAD
-    #Advance to the next line
+#Iterate through all lines in linelist
+for lineString in line_list:
     if lineString[0] in ("#",'u'):
-                 #Read the next line
-                 lineString = file_object.readline()
                  continue
 
     #Split the string into a list of data items
@@ -34,17 +35,17 @@ while lineString:
     #Extract the items in list into variables
     record_id = lineData[0]  # ARGOS tracking record ID
     obs_date = lineData[2]   # Observation date
-    ob_lc = lineData[4]      # Observation Location Class
+    obs_lc = lineData[4]      # Observation Location Class
     obs_lat = lineData[6]    # Observation Latitude
     obs_lon = lineData[7]    # Observation Longitude
     
-    print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+    if obs_lc in ("1","2","3"):
+        #print(f"Record {record_id} indicated Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+        date_dict[record_id] = obs_date
+        coord_dict[record_id] = (obs_lat, obs_lon)
     
-    #Read the next line
-    lineString = file_object.readline()
-
-#Close the file
-file_object.close()
+print(date_dict)
+print(coord_dict)
 
 # =============================================================================
 #
@@ -128,6 +129,44 @@ file_object.close()
 # print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
 # 
 # =============================================================================
+=======
+#Pretend we read one line of data from the file
+lineString = line_list[3]
+print(lineString)
+
+#Split the string into a list of data items
+lineData = lineString.split()
+
+#Extract items in list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat},lon:{obs_lon} on {obs_date}")
+
+##################################
+
+# Copy and paste a line of data as the lineString variable value
+lineString = "20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0"
+
+# Use the split command to parse the items in lineString into a list object
+lineData = lineString.split()
+lineData[3]
+
+# Assign variables to specfic items in the list
+record_id = lineData[0]   # ARGOS tracking record ID
+obs_date = lineData[2]   # Observation date
+ob_lc = lineData[3]       # Observation Location Class
+obs_lat = lineData[6]     # Observation Latitude
+obs_lon = lineData[7]     # Observation Longitude
+
+# Print information to the use
+print (f"Record {record_id} indicates Sara was seen at {obs_lat}N and {obs_lon}W on {obs_date}")
+
+>>>>>>> parent of 0f5b96d (iterate through all records)
 =======
 #Pretend we read one line of data from the file
 lineString = line_list[3]
